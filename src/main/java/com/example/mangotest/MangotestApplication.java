df0f74ea315a8ec216a5041f07a46ec4bfc17a34
Ekmadish <estaykargabay1206@gmail.com>
2023-11-05 22:35:37 +0600

    Initial commit

---
+++ src/main/java/com/example/mangotest/MangotestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangotestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mangotest/MangotestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
